35fc23be105b96168a0303e54f96e87acd499927
oxaid <id.oxa.std@gmail.com>
2025-06-19 18:36:20 +0700

    update

--- braintrust/src/components/Footer.jsx
+++ braintrust/src/components/Footer.jsx
@@ -3,11 +3,18 @@ import iconX from "../assets/icons/x.svg"
 import iconGithub from "../assets/icons/github.svg"
 import iconTelegram from "../assets/icons/telegram.svg"
 import arrowRightTop from "../assets/icons/arrow-right-top.svg"
+import { useLocation } from "react-router-dom";
 
 export default function Footer() {
+  const location = useLocation();
+  const hiddenPaths = ["/privacy-policy", "/terms-of-services", "/contact"];
+  const isHiddenPage = hiddenPaths.includes(location.pathname.toLowerCase());
+
   return (
     <footer className=" text-white font-mono px-6 md:px-16 py-16">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-12 gap-10 items-start">
+      <div className={`max-w-7xl ${isHiddenPage ? "md:max-w-5xl" : ""} mx-auto grid grid-cols-1 md:grid-cols-12 gap-10 items-start`}>
+      {/* <div className={`max-w-7xl mx-auto grid grid-cols-1 ${isHiddenPage ? "md:grid-cols-1" : "md:grid-cols-12"} gap-10 items-start`}> */}
+
 
         {/* LEFT SECTION */}
         <div className="md:col-span-4 flex flex-col gap-5">
@@ -41,6 +48,10 @@ export default function Footer() {
         </div>
 
         {/* CENTER SECTION */}
+        {isHiddenPage ? (
+        <div className="md:col-span-5" aria-hidden="true"></div>
+        ) : (
+
        <div className="md:col-span-5 grid grid-cols-2 gap-y-3 gap-x-8 text-xs text-white/80">
           <a href="#" className="hover:underline transition">CAREERS</a>
           <a href="#howitworks" className="hover:underline transition">HOW IT WORKS</a>
@@ -49,6 +60,7 @@ export default function Footer() {
           <a href="/privacy-policy" className="hover:underline transition">PRIVACY POLICY</a>
           <a href="/terms-of-services" className="hover:underline transition">TERMS OF SERVICES</a>
         </div>
+        )}
 
         {/* RIGHT SECTION */}
         <div className="md:col-span-3 flex flex-col items-end justify-between h-full gap-4">
